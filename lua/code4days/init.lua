c6ea6d6561b27348bc3fd8cbc06ce662899fb5a8
Rasheed Elsaleh <rasheed.elsaleh@octo.us>
2024-12-23 22:56:27 -0500

    add more plugins; add settings; add autocmds

--- lua/code4days/init.lua
+++ lua/code4days/init.lua
@@ -1,2 +1,7 @@
 require("code4days.remap")
 require("code4days.lazy")
+require("code4days.settings")
+require("code4days.auto")
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
