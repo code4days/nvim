cfe96c303f9e7a3d59f1a037e755ab56951f2679
Rasheed <relsaleh@gmail.com>
2024-12-24 21:38:56 -0500

    add more remaps

--- lua/code4days/remap.lua
+++ lua/code4days/remap.lua
@@ -36,3 +36,17 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Move highlighted text up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Set the cursor to the beginnnig of the line with J
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep the cursor in the same place when moving half page up or down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+--Paste in visual mode w/o losing the current paste buffer
+vim.keymap.set("x", "<leader>p", '"_dP')
